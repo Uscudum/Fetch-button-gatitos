e79b9d6dbc63c68af533ebacaa2749912dd1f9ec
Guillermo Uscudum <guilleuscudum@gmail.com>
2022-08-18 11:46:00 -0300

    clic actualizado

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const URL = 'https://api.thecatapi.com/v1/images/search';
 const button = document.getElementById("click");
 
-async function clic(){ 
+function clic(){ 
 
- const res = await fetch(URL)
- const respuesta = await res.json()
- const img = document.querySelector('img');
- img.src = respuesta[0].url;
-
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        const img = document.querySelector('img');
+        img.src = data[0].url;
+    })
 }
 
 button.onclick = clic;
